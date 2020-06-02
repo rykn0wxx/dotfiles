03aa336a008f4596feb345d6dfe3f72fec3ea07a
rykn0wxx <raurici666@gmail.com>
2020-06-02 08:06:16 -0400

    added yarn

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -2,6 +2,8 @@
 
 curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 sleep 1
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sleep 1
 
 echo "##" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
@@ -13,5 +15,7 @@ deb-src https://deb.nodesource.com/node_12.x buster main
 ##
 EOF
 
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 sudo apt update
-sudo apt install nodejs -y
+sudo apt install nodejs yarn -y
